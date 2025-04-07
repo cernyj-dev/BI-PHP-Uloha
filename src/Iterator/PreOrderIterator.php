3191b340623e3f3d871ec4f3396101f819517c96
Jakub Černý <cer.kubas@gmail.com>
2025-04-07 10:38:00 +0200

    Update PreOrderIterator.php

--- src/Iterator/PreOrderIterator.php
+++ src/Iterator/PreOrderIterator.php
@@ -4,9 +4,9 @@ namespace Iterator;
 
 use Node;
 
+// Polymorphed PreOrder iterator to traverse the tree data structure in a PreOrder manner
 class PreOrderIterator extends AbstractOrderIterator
 {
-    // TODO specific methods
     public function __construct(Node $root)
     {
         parent::__construct($root);
